dfc5874d77b3a40a6a10e1eda53c0f85722f9366
mennitise <mennitise@gmail.com>
2016-08-31 16:51:57 -0300

    Soluciono warnings de checklist - 2

--- src/test/java/ar/fiuba/tdd/template/MyLinkedListTests.java
+++ src/test/java/ar/fiuba/tdd/template/MyLinkedListTests.java
@@ -2,41 +2,36 @@ package ar.fiuba.tdd.template;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MyLinkedListTests {
 
     @Test
-    public void linkedListNewIsEmpty()
-    {
+    public void linkedListNewIsEmpty() {
         assertTrue((new MyLinkedList<String>().isEmpty()));
     }
 
     @Test
-    public void linkedListNewHaveZeroElements()
-    {
+    public void linkedListNewHaveZeroElements() {
         assertEquals((new MyLinkedList<String>().size()),0);
     }
 
-    private MyLinkedList<String> setListOneItem()
-    {
+    private MyLinkedList<String> setListOneItem() {
         MyLinkedList<String> list = new MyLinkedList<>();
-        String s = "hola mundo";
-        list.addFirst(s);
+        String words = "hola mundo";
+        list.addFirst(words);
         return list;
     }
 
     @Test
-    public void linkedListAddElementOneElement()
-    {
+    public void linkedListAddElementOneElement() {
         MyLinkedList<String> list = this.setListOneItem();
         assertEquals(list.size(),1);
     }
 
     @Test
-    public void linkedListAddElementSameElement()
-    {
+    public void linkedListAddElementSameElement() {
         MyLinkedList<String> list = this.setListOneItem();
         assertEquals(list.getFirst(),"hola mundo");
     }
--- src/test/java/ar/fiuba/tdd/template/MyQueueTests.java
+++ src/test/java/ar/fiuba/tdd/template/MyQueueTests.java
@@ -2,48 +2,42 @@ package ar.fiuba.tdd.template;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MyQueueTests {
 
     @Test
-    public void queueNewIsEmpty()
-    {
+    public void queueNewIsEmpty() {
         assertTrue((new MyQueue<String>().isEmpty()));
     }
 
     @Test
-    public void queueNewHaveZeroElements()
-    {
+    public void queueNewHaveZeroElements() {
         assertEquals((new MyQueue<String>().size()),0);
     }
 
-    private MyQueue<String> setQueueOneItem()
-    {
+    private MyQueue<String> setQueueOneItem() {
         MyQueue<String> queue = new MyQueue<>();
-        String s = "elemento1";
-        queue.add(s);
+        String word = "elemento1";
+        queue.add(word);
         return queue;
     }
 
     @Test
-    public void queueAddElementOneElement()
-    {
+    public void queueAddElementOneElement() {
         MyQueue<String> queue = this.setQueueOneItem();
         assertEquals(queue.size(),1);
     }
 
     @Test
-    public void queueAddElementSameElement()
-    {
+    public void queueAddElementSameElement() {
         MyQueue<String> queue = this.setQueueOneItem();
         assertEquals(queue.top(),"elemento1");
     }
 
     @Test
-    public void queueAddAnElementAndRemoveQueueEmpty()
-    {
+    public void queueAddAnElementAndRemoveQueueEmpty() {
         MyQueue<String> queue = this.setQueueOneItem();
         queue.remove();
         assertTrue(queue.isEmpty());
